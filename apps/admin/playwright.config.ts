8f6defeba8681fbaefe11de0026ae1b84dc52bc7
Damian Kądziela <damiankadziela4@gmail.com>
2025-09-02 14:15:03 +0200

    feat(admin): initialize payloadcms admin app

---
+++ apps/admin/playwright.config.ts
@@ -0,0 +1,37 @@
+import { devices } from '@playwright/test';
+import { createConfig } from '@repo/playwright-config/base';
+
+export default createConfig({
+	testDir: './e2e',
+	use: {
+		baseURL: 'http://localhost:5001',
+	},
+	webServer: {
+		command: 'pnpm run dev',
+		port: 5000,
+		reuseExistingServer: !process.env.CI,
+		timeout: 120 * 1000,
+	},
+	projects: [
+		{
+			name: 'setup',
+			testMatch: /.*\.setup\.ts/,
+		},
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] },
+			dependencies: ['setup'],
+		},
+		{
+			name: 'firefox',
+			use: { ...devices['Desktop Firefox'] },
+			dependencies: ['setup'],
+		},
+		{
+			name: 'smoke-chromium',
+			use: { ...devices['Desktop Chrome'] },
+			testMatch: /.*\.smoke\.spec\.ts/,
+			dependencies: ['setup'],
+		},
+	],
+});
